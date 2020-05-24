c28d29630e48469d59ff37d214a147a2ec550f87
anuragkrsrivastava <anuragkrsrivastava05@gmail.com>
2020-05-24 18:55:11 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,111 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Button,
+  Image,
+} from 'react-native';
+
+
+const App = () => {
+
+  state = {
+    data: [],
+    title: null,
+    year: null
+  };
+
+  onChangeText = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit = () => {
+    const title = this.state.title;
+    const year = this.state.year;
+    if((year != null) && (title != null)){
+      fetch('http://www.omdbapi.com/?t=${title}&y=${year}', {method: 'GET'})
+      .then(res => res.json())
+      .then(movie => this.setState({data: movie}));
+    }
+  }
+
+  DataPass = (item, yr) => {
+    return (
+      <Movie post={item} time={yr}></Movie>
+    );
+  }
+  return(
+      <View>
+      <View>
+        <FormLabel>Title</FormLabel>
+        <FormInput name="title" onChangeText={onChangeText}></FormInput>
+        <FormValidationMessage>This Field is Required</FormValidationMessage>
+        <FormLabel>Year</FormLabel>
+        <FormInput name="year" onChangeText={onChangeText}></FormInput>
+        <FormValidationMessage>This Field is Required</FormValidationMessage>
+        <Button type="submit" class="btn btn-success" onPress={onSubmit}></Button>
+      </View>
+      <View>
+        <ScrollView style={styles.scrollView}>
+        <FlatList data={this.state.data} renderItem = {({data}) => (<View><Image source = {require(data.Poster)} style={styles.img}></Image>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name={data.Title}
+                component = {Movie}
+                onPress = {() => DataPass(this.state.title, this.state.year)}>
+              </Stack.Screen>
+            </Stack.Navigator>
+          </NavigationContainer>{data.Year}</View>)}> </FlatList>
+        </ScrollView>
+      </View>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  img: {
+    width: 50,
+    height: 80,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ Movie.js
@@ -0,0 +1,39 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Button,
+  Image,
+  FlatList,
+} from 'react-native';
+
+const Movie = ({post, time}) =>{
+    state = {
+        data: []
+    };
+    const movie = fetch('http://www.omdbapi.com/?t=${post}&y=${time}&plot=full', {method: 'GET'}).then(res =>res.json()).then(item => this.setState({data: item}));
+    return(
+        <View>
+            <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name={Home}
+                component = {App}>
+              </Stack.Screen>
+            </Stack.Navigator>
+          </NavigationContainer>
+          <View>
+              {movie}
+              <Image source = {require(this.state.data.Poster)}></Image>
+              <Text>{this.state.data.Title}</Text>
+              <Text>{this.state.data.Year}</Text>
+              <Text>{this.state.data.Director}</Text>
+              <FlatList data={this.state.data.Actors} renderItem = {({actor}) => <Text>{actor}</Text>}></FlatList>
+          </View>
+        </View>
+    )
+}
+
+export default Movie;
